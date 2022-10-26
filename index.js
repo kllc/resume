a01e5c02621e7103399307395534d04dcb30c37f
K <kijima@kllc.jp>
2022-10-26 23:29:18 +0900

    index

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 export default () => {
   const index = [
     {
-      src: "encode.html",
-      anchor: true,
-      img: "https://media.springernature.com/w580h326/nature-cms/uploads/collections/NSUP_Cov1-8a789cb352586f4994750d169523aa03.jpg",
-    },
-    {
       src: "k.md",
       date: "22.10.16",
       title: "木島 基文 経歴書",
       img: "https://kllc.jp/images/kijima.png",
+    },
+    {
+      src: "encode.html",
+      anchor: true,
+      img: "https://media.springernature.com/w580h326/nature-cms/uploads/collections/NSUP_Cov1-8a789cb352586f4994750d169523aa03.jpg",
     },
     {
       src: "sample.md",
